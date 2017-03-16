d12c11fe8d0190efa1d541341971f4932cd6a18a
msrgnt <sarg.lomb@gmail.com>
2017-03-15 19:49:31 -0700

    toggleclass on search

--- js/app.js
+++ js/app.js
@@ -3,3 +3,9 @@
   Michael Lombardo
   Please add all Javascript code to this file.
 */
+
+$("#search").click( function(event){
+  event.preventDefault();
+  $("#search").toggleClass("active");
+  console.log("s#searchearch");
+});

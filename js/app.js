797b9b4efee72e13aac8ea8e2238088a34436da7
msrgnt <sarg.lomb@gmail.com>
2017-03-16 11:17:10 -0700

    have titles populating headers

--- js/app.js
+++ js/app.js
@@ -4,51 +4,33 @@
   Please add all Javascript code to this file.
   Got data working
 */
-$(document).ready(function(){
-
-$('h3').on('click', function() {
-    console.log("testing");
-    $('#popUp').show().removeClass('hidden');
-
-});
-
-$('.closePopUp').on('click', function(){
-    $('#popUp').hide();
-});
-
-
-});
-
-
-$(".first-link").click(function(){
+$(window).load(function() {
   $.get("https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json", function(results){
-    console.log(results);
+    var titleDigg = results.data.feed[0].content.title;
+    $(".first-h").text(titleDigg);
+    //console.log(results);
   });
-console.log("hello");
-});
 
-$(".second-link").click(function(){
   $.get("https://accesscontrolalloworiginall.herokuapp.com/http://thedailywtf.com/api/articles/random", function(resultsB){
-    console.log(resultsB);
+    $(".second-h").text(resultsB.Title);
+    //console.log(resultsB);
   });
-console.log("hello");
-});
 
-$(".third-link").click(function(){
   $.get("https://accesscontrolalloworiginall.herokuapp.com/https://newsapi.org/v1/articles?source=techcrunch&apiKey=a95641f41d1343b6b44c9c7f0e8aef2e", function(resultsC){
-    console.log(resultsC);
+    $(".third-h").text(resultsC.articles[0].title);
+    //console.log(resultsC);
   });
-console.log("hello");
-});
 
-$(".fourth-link").click(function(){
   $.get("https://accesscontrolalloworiginall.herokuapp.com/https://content.guardianapis.com/search?q=atlanta%20OR%20poop%20bacon&from-date=2017-01-01&api-key=9be4a5cd-ef5e-4299-b2be-20427c6ca171", function(resultsD){
-    console.log(resultsD);
+    $(".fourth-h").text(resultsD.response.results[0].webTitle);
+    //console.log(resultsD);
   });
-console.log("hello");
-});
 
-$(".fifth-link").click(function(){
+  $.get("https://accesscontrolalloworiginall.herokuapp.com/http://www.reddit.com/r/all.json", function(resultsE){
+    //$(".fourth-h").text(resultsD.response.results[0].webTitle);
+    $(".fifth-h").text(resultsE.data.children[0].data.title);
+  });
+
   $.getJSON(
         "http://www.reddit.com/r/all.json?jsonp=?",
         function foo(data)
@@ -56,19 +38,58 @@ $(".fifth-link").click(function(){
           $.each(
             data.data.children.slice(0, 10),
             function (i, post) {
-              console.log(post.data.title );
-              console.log(post.data.url );
-              console.log(post.data.permalink );
-              console.log(post.data.ups );
-              console.log(post.data.downs );
+            //  console.log(post.data.title );
+              //console.log(post.data.url );
+              //console.log(post.data.permalink );
+              //console.log(post.data.ups );
+              //console.log(post.data.downs );
 
             }
           )
         }
       )
-      .success(function() { alert("second success"); })
-      .error(function() { alert("error"); })
-      .complete(function() { alert("complete"); });
+
+          //.success(function() { alert("second success"); })
+        //  .error(function() { alert("error"); })
+          //.complete(function() { alert("complete"); });
+});
+
+$(document).ready(function(){
+  $("#popup").addClass(".loader");
+  console.log("poop");
+
+  $('h3').on('click', function() {
+      console.log("testing");
+      $('#popUp').show().removeClass('hidden');
+  });
+  $('.closePopUp').on('click', function(){
+      $('#popUp').hide();
+  });
+});
+
+$(".first-link").click(function(){
+  console.log("hello");
+
+});
+
+$(".second-link").click(function(){
+  console.log("hello");
+
+});
+
+$(".third-link").click(function(){
+  console.log("hello");
+
+});
+
+$(".fourth-link").click(function(){
+
+console.log("hello");
+});
+
+$(".fifth-link").click(function(){
+  console.log("hello");
+
 });
 
 //9be4a5cd-ef5e-4299-b2be-20427c6ca171
@@ -81,10 +102,6 @@ $("#search").click(function(event){
   console.log("s#searchearch");
 });
 
-$(document).ready(function(){
-  $("#popup").addClass(".loader");
-  console.log("poop");
-});
 
 $("container").click(function(event){
   event.preventDefault();
